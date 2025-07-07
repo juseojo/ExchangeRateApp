62bf6ba71ab2da85ecbfd95e586d9f7dcbfcdae1
juseojo <j86371224@gmail.com>
2025-07-07 10:20:49 +0900

    Feat: 메인 UI 베이스 생성

---
+++ exchangeRateApp/Presentation/Screens/View/MainView.swift
@@ -0,0 +1,38 @@
+//
+//  MainView.swift
+//  exchangeRateApp
+//
+//  Created by seongjun cho on 7/4/25.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class MainView: UIView {
+	lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: makeCollectionViewLayout()).then {
+		$0.backgroundColor = .systemBackground
+		$0.showsVerticalScrollIndicator = false
+	}
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+
+		addSubview(collectionView)
+		collectionView.snp.makeConstraints { make in
+			make.edges.equalToSuperview()
+		}
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	private func makeCollectionViewLayout() -> UICollectionViewLayout {
+		let config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+		let layout = UICollectionViewCompositionalLayout.list(using: config)
+
+		return layout
+	}
+}
--- exchangeRateApp/Presentation/Screens/ViewController/MainViewController.swift
+++ exchangeRateApp/Presentation/Screens/ViewController/MainViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class MainViewController: UIViewController {
+	
+	let mainView = MainView()
+
+	override func loadView() {
+		view = mainView
+	}
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
 	}
 
 
