d086ab7676c107f54e7f4575dc5e9ba1842cb6ed
juseojo <j86371224@gmail.com>
2025-07-08 11:30:00 +0900

    Feat: 데이터 통신 구현

---
+++ exchangeRateApp/Presentation/Screens/Model/ExchangeRateItem.swift
@@ -0,0 +1,11 @@
+//
+//  ExchangeRateItem.swift
+//  exchangeRateApp
+//
+//  Created by seongjun cho on 7/7/25.
+//
+
+struct ExchangeRateItem: Hashable {
+	var countryCode: String
+	var rate: String
+}
